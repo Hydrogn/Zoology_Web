00bc4c7a7f586e920b0f3246bba6f6f5e6bb695c
Hydro <luisanthonynuyda.grado@bicol-u.edu.ph>
2022-12-08 22:08:38 +0800

    awdwa

--- function.js
+++ function.js
@@ -2,6 +2,6 @@ function setStorage(name, info, img){
     sessionStorage.setItem("name", name);
     sessionStorage.setItem("info", info);
     sessionStorage.setItem("img", img);
-    window.location.href = "../animalInfo.html";
+    window.location.href = "../gallery/animalInfo.html";
 }
 
